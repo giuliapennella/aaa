a35701656ba3aa056d040a93b6578a2c0b844e3e
elenab19 <elena.brugnolo@mail.polimi.it>
2024-06-25 01:11:38 +0200

    scene3

--- src/main/java/com/example/gui/ControllerScena3.java
+++ src/main/java/com/example/gui/ControllerScena3.java
@@ -1,12 +1,41 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import java.util.Objects;
+
 public class ControllerScena3 extends ControllerScene {
 
+    @FXML
+    public Button button00;
+    public GridPane gridPane;
+    public ImageView imageView00;
     Main main;
+
+    Image image00 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/gui/backCard12.png")));
 
 
     public void setMain(Main main){
         this.main=main;
     }
 
+    @FXML
+    public void button00Clicked(ActionEvent event) {
+        button00.setDisable(true);
+        System.out.println("button00 clicked");
+        imageView00 = new ImageView(image00);
+
+        // Assicurati che l'ImageView si adatti alla cella
+        imageView00.setFitWidth(100);
+        imageView00.setFitHeight(200);
+        imageView00.setPreserveRatio(true);
+
+        gridPane.add(imageView00, 0, 0);
+
+    }
 }
