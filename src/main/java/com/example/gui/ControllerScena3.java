c950397ffa5825b6570eef643d86286d96f21995
giulia <giulia.pennella1@gmail.com>
2024-06-25 02:04:21 +0200

    gridpane

--- src/main/java/com/example/gui/ControllerScena3.java
+++ src/main/java/com/example/gui/ControllerScena3.java
@@ -3,6 +3,7 @@ package com.example.gui;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import java.util.Objects;
@@ -13,36 +14,35 @@ public class ControllerScena3 extends ControllerScene {
 
     @FXML
     GridPane gridPane;
+    Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/gui/backCard12.png")));
 
+    Image image0 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/gui/backCard00.png")));
     @FXML
     public void addButtonGridPane(){
 
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/gui/backCard12.png")));
-
+        int x,y;
         for (int i = 0; i < 14; i++) {
             for (int j = 0; j < 14; j++) {
-                if(i%2==0)
-                {
-                    if(j%2==0)
-                    {
-                        ImageView imageView = new ImageView(image);
-                        imageView.setFitWidth(200);
-                        imageView.setFitHeight(100);
-                        imageView.setPreserveRatio(true);
-                        gridPane.add(imageView, j, i);
-                    }
-                }else {
-                    if(j%2==1)
-                    {
-                        ImageView imageView = new ImageView(image);
-                        imageView.setFitWidth(200);
-                        imageView.setFitHeight(100);
-                        imageView.setPreserveRatio(true);
-                        gridPane.add(imageView, j, i);
-                    }
-                }
+                ImageView imageView = new ImageView(image0);
+                imageView.setFitWidth(200);
+                imageView.setFitHeight(100);
+                imageView.setPreserveRatio(true);
+                gridPane.add(imageView, j, i);
+                x=j;
+                y=i;
+                int finalY = y;
+                int finalX = x;
+                imageView.setOnMouseClicked((MouseEvent event) -> {placeCard(finalX, finalY);});
             }
         }
+    }
+
+    private void placeCard(int x,int y) {
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(100);
+        imageView.setPreserveRatio(true);
+        gridPane.add(imageView, x, y);
     }
 
     public void setMain(Main main){
